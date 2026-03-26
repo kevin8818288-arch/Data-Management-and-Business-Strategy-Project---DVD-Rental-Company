6a900dedb341c0f39be069e678c10a0c039bb54b
Kevin Yu <kevin8818288@gmail.com>
2026-03-26 17:47:50 -0500

    Update 01_data_extraction.sql

--- sql/01_data_extraction.sql
+++ sql/01_data_extraction.sql
@@ -6,3 +6,42 @@
   - language l
   - film category fc
   - rental r
+
+WITH stg_film AS (
+    SELECT
+        film_id,
+        title AS film_title,
+        rental_rate,
+        replacement_cost
+    FROM film
+),
+stg_inventory AS (
+    SELECT
+        inventory_id,
+        film_id
+    FROM inventory
+),
+stg_rental AS (
+    SELECT
+        rental_id,
+        inventory_id,
+        rental_date
+    FROM rental
+),
+stg_payment AS (
+    SELECT
+        payment_id,
+        rental_id,
+        amount,
+        payment_date
+    FROM payment
+    WHERE amount >= 0
+),
+stg_category AS (
+    SELECT
+        fc.film_id,
+        c.name AS category_name
+    FROM film_category fc
+    JOIN category c
+      ON fc.category_id = c.category_id
+)
